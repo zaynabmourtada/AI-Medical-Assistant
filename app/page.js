16110320fe07b43e0047801f3e07c82a4dc419cb
Zaynab Mourtada <zaynabmo@umich.edu>
2024-08-28 22:59:41 -0400

    Modified app/page.js - handles user's message to server

--- app/page.js
+++ app/page.js
@@ -12,7 +12,54 @@ export default function Home() {
   ])
   const [message, setMessage] = useState('')
   const sendMessage = async () => {
+    if (!message.trim()) return;
 
+    setMessage('')
+    setMessages((messages) => [
+      ...messages,
+      { role: 'user', content: message },
+      { role: 'assistant', content: '' }
+    ])
+
+    try {
+      const response = await fetch('/api/chat', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([...messages, { role: 'user', content: message }]),
+      })
+
+      if (!response.ok) {
+        throw new Error('Network response was not okay.')
+      }
+
+      const reader = res.body.getReader()
+      const decoder = new TextDecoder()
+
+      while (true) {
+        const { done, value } = await reader.read()
+        if (done) break
+        const text = decoder.decode(value, { stream: true })
+        setMessages((messages) => {
+          let lastMessage = messages[messages.length - 1]
+          let otherMessages = messages.slice(0, messages.length - 1)
+          return [
+            ...otherMessages,
+            {
+              ...lastMesssage, content: lastMesssage.content + text
+            },
+          ]
+        })
+      }
+    }
+    catch (error) {
+      console.error('Error:', error)
+      setMessages((messages) => [
+        ...messages,
+        { role: 'medical assistant', content: "I am sorry, but I encountered an error. Please try again." }
+      ])
+    }
   }
 
   return (
@@ -38,18 +85,18 @@ export default function Home() {
           flexGrow={1}
           overflow="auto"
           maxHeight="100%"
-          >
-            {messages.map((message, index) => (
+        >
+          {messages.map((message, index) => (
             <Box
               key={index}
               display="flex"
               justifyContent={
-                message.role === 'Medical Assistant' ? 'flex-start' : 'flex-end'
+                message.role === 'medical assistant' ? 'flex-start' : 'flex-end'
               }
             >
               <Box
                 bgcolor={
-                  message.role === 'Medical Assistant'
+                  message.role === 'medical assistant'
                     ? 'primary.main'
                     : 'secondary.main'
                 }
@@ -72,7 +119,7 @@ export default function Home() {
           <Button variant="contained" onClick={sendMessage}>
             Send
           </Button>
-          </Stack>
+        </Stack>
       </Stack>
     </Box>
 
